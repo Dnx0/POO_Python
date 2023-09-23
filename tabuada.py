71cbfc2b740ec409f30b529d4627a0c59335e982
Victor Pereira <82619763+Dnx0@users.noreply.github.com>
2023-09-23 17:22:46 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,5 @@
-numero = int(input('Digite o número que vai ser feito a tabuada: '))
-tabuada = 10
+multiplicando = int(input('Digite o número que vai ser feito a tabuada: '))
+multiplicador = 10
 
-for i in range(1, tabuada + 1):
-    print(numero * i)
+for i in range(1, multiplicador + 1):
+    print(multiplicando * i)
